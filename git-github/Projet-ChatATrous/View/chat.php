e4aecf28f97be418477030fc233dff955c407969
ABDOULAYE CONDE <colenthin@gmail.com>
2021-06-03 11:59:10 +0200

    commit getDBConnection() / findAll()

--- git-github/Projet-ChatATrous/View/chat.php
+++ git-github/Projet-ChatATrous/View/chat.php
@@ -26,17 +26,17 @@
         </tr>
     </thead>
     <tbody>
-
-    
+        
     <!-- Dans une premier temps faites une boucle pour afficher "en dur" plusieurs messages (plusieurs fois le même) -->
      <?php 
-      for($i=0 ; $i<5;$i++)
-      { ?>
+
+foreach($tab_affi=findAll() as $row) {
+  ?>
         <tr class="table-light">
-        <td class="col-2">01/06/2021</td>
-        <td class="col-2">Camile</td>
-        <td class="col-8">mon message</td>
-    </tr>
+        <td class="col-2"><?=($row["date"])?></td>
+        <td class="col-2"><?=($row["pseudo"])?></td>
+        <td class="col-8"><?=($row["content"])?></td>
+        </tr>
      
 <?php }
      ?>       
--- git-github/Projet-ChatATrous/model/model.php
+++ git-github/Projet-ChatATrous/model/model.php
@@ -1,68 +1,24 @@
 <?php
-  //connection;
-$user="root";
-$pass="";
-$dbname="chat";
-$host="localhost";
 
-try {
+  //define connection function
+  function getDBConnection():PDO{
+  $user="root";
+  $pass="";
+  $dbname="chat";
+  $host="localhost";
 
-    $dns= 'mysql:host='.$host.';dbname='.$dbname;
-        //echo $dns;
-    $options=array(
-PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8",
-PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION
+  $options=array(
+    PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8",
+    PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION
+        );
 
-    );
-    $dbh = new PDO($dns, $user, $pass,$options);
-   //var_dump( $dbh);
-   //echo "connexion etablie";
-   
-} catch (PDOException $e) {
-    print "Erreur connexion !: " . $e->getMessage() . "<br/>";
-    die();
-}
+ $dns= 'mysql:host='.$host.';dbname='.$dbname; 
+ $dbh = new PDO($dns, $user, $pass,$options); 
 
-// findall
-
-try {
-       
-    $query= 'SELECT id, pseudo, content, date FROM message ORDER BY date DESC LIMIT 5;';
-    $req = $dbh->query($query);
-    $req->setFetchMode(PDO::FETCH_ASSOC);
-    $tab = $req->fetchAll();
-    $req ->closeCursor();
-
-//var_dump($tab );  
-    $dbh = null; 
-    echo "fin de requete";
-} catch (PDOException $e) {
-    print "Erreur !: " . $e->getMessage() . "<br/>";
-    die();
-}
-?>
-
-<!DOCTYPE html>
-<html lang="fr">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>ici mon titre</title>
-        <link rel="stylesheet" href="https://bootswatch.com/5/lumen/bootstrap.min.css">
-    </head>
-
-<body>
-
- <?php foreach($tab as $row) { ?>
-    <p> <?= $row["pseudo"]?> ......</p>
-    <p> <?= $row["content"]?> </p> 
-<?php }?> 
-
-</body>
-<html>
-
+  return $dbh;
+  
+  }
+?> 
 
 <?php
 
@@ -70,11 +26,17 @@ try {
  * Récupérer les messages dans la base de données
  */
 function findAll(): array
-{
-    $db = getDBConnection();
+{ 
+    $dbh = getDBConnection();
+    $sth = $dbh->query('SELECT date,pseudo,content FROM message');
+    $sth->setFetchMode(PDO::FETCH_ASSOC);
+    $tab = $sth->fetchAll();
+    $sth ->closeCursor();    
+    
+    return $tab;
 
-    // Coder ici
 }
+
 
 /**
  * Ajouter un message dans la base de données
@@ -88,8 +50,9 @@ function create(array $post): void
 
 /**
  * Connection à la base de donnéess
+ * function getDBConnection(): PDO
+*{
+*    // Coder ici
+*}
  */
-function getDBConnection(): PDO
-{
-    // Coder ici
-}
+
